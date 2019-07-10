aa43f2a979c8f041189a3f1e94fc60a308f745ba
seaHung <45138439+seaHung@users.noreply.github.com>
2019-07-11 00:10:51 +0800

    add

---
+++ src/main/java/cn/tellsea/skeleton/business/mapper/TbUserMapper.java
@@ -0,0 +1,7 @@
+package cn.tellsea.skeleton.business.mapper;
+
+import cn.tellsea.core.base.mapper.MyMapper;
+import cn.tellsea.skeleton.business.entity.TbUser;
+
+public interface TbUserMapper extends MyMapper<TbUser> {
+}
---
+++ src/main/java/cn/tellsea/skeleton/business/service/TbUserService.java
@@ -0,0 +1,13 @@
+package cn.tellsea.skeleton.business.service;
+
+import cn.tellsea.skeleton.core.base.service.BaseService;
+import cn.tellsea.skeleton.entity.TbUser;
+
+/**
+* @Description: TbUser 接口
+* @author tellsea
+* @date 2019/07/11 00:04
+*/
+public interface TbUserService extends BaseService<TbUser> {
+
+}
